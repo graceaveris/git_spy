a86228dd6614d13d598d401da2a85626e6533e8e
Grace Averis <39510617+graceaveris@users.noreply.github.com>
2020-05-26 11:54:33 +0200

    syntax tidy

--- carousel.js
+++ carousel.js
@@ -1,24 +1,24 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1
+showSlides(slideIndex)
 
 //Will pass in 1 or -1
 function changeSlide(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n)
   };
 
 //Passes in the index of active slide
 function showSlides(index) {
   let i;
-  let slides = document.getElementsByClassName('carousel__slide');
+  let slides = document.getElementsByClassName('carousel__slide')
 
   //keeps the carousel going in a loop
-  if (index > slides.length) { slideIndex = 1 };   
-  if (index < 1) { slideIndex = slides.length };
+  if (index > slides.length) { slideIndex = 1 }  
+  if (index < 1) { slideIndex = slides.length }
 
   //hides all images
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none"  
   };
   //reveals the actve image by adding class block
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block"  
 }
